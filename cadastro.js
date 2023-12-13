773be7799cab7a90b6ac25639063a581cdb2b68d
devruy <adriano.oliveira@BRAIN.local>
2023-12-13 11:07:42 -0300

    adicionando funcionalidade de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -2,4 +2,11 @@ funcition signup(email) {
     return email;
 } 
 
-// cadastrando um email na plataforma.
+// cadastrando um email na plataforma. 
+
+funcition signup(email) {
+    return "Bem vindo ao sitema";
+}  
+
+
+// adicionado funcao de mansagem 
